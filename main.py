f7ac573881799fe02dbdc4c921a2388b07e818d2
chloeleang-dev <chloe.leang@edu.esiee.fr>
2025-11-13 10:55:42 +0000

    travail

--- main.py
+++ main.py
@@ -1,20 +1,55 @@
+"""
+module premier
+"""
 from math import sqrt
+
+
 
 #### Fonction secondaire
 
 
 def isprime(p):
+    """
+    
+    Vérifier si un entier est un nombre premier ou pas.
+    
+    Args:
+        t: nombre entier 
+        p: nombre entier
 
-    # votre code ici
+    Returns:
+        True ou False
+    """
 
-    pass
+    if p <= 1:
+        return False
+    t = int(sqrt(p)) + 1
+    premier = True
+
+    while t>2:
+        t = t-1
+        if p%t == 0:
+            premier = False
+
+    # if premier == True:
+    #     return True
+    # else:
+    #     return False
+
+    return premier
 
 #### Fonction principale
 
 
 def main():
 
+    """
+    main 
+    """
+
     # vos appels à la fonction secondaire ici
+    print(isprime(8))
+    print(isprime(7))
 
     for n in range(100):
         if isprime(n):
